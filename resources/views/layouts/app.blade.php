d09b0d408380d623ca8aa55e216675ae2a9d509f
PNSB\zharif.rosli <zharif.rosli@puncakniaga.com.my>
2025-01-24 16:51:53 +0800

    Initial Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+
+    <title>{{ config('app.name', 'Base App') }}</title>
+
+    @viteReactRefresh
+    @vite(['resources/js/main.jsx'])
+
+    <script type="text/javascript">
+        window.Laravel = {
+            csrfToken: "{{ csrf_token() }}",
+        }
+    </script>
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Fallback URL
+Route::get('/{any?}', function () {
+    return view('layouts.app');
+})->where('any', '^(?!api\/)[\/\w\.\,-]*');
+
+
+require __DIR__.'/auth.php';
