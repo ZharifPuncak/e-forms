fe462779aa95921850accf6650d0b95ddb5cde16
ZharifPuncak <zharif.rosli@puncakniaga.com.my>
2025-03-13 16:10:45 +0800

    Added report controller

--- resources/js/page-sections/dashboard/forms/views/form-table.jsx
+++ resources/js/page-sections/dashboard/forms/views/form-table.jsx
@@ -18,65 +18,69 @@ import { CheckCircle as CheckCircleIcon } from "@phosphor-icons/react/dist/ssr/C
 import { paths } from "@/paths";
 
 import useAxios  from "@/hooks/use-axios";
-
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export function FormTable() {
 
     const { axiosGet, axiosMutate } = useAxios();
 	const navigate = useNavigate();
 	const { isLoading, data : forms, refetch   }  = axiosGet({  id : 'forms' , url : import.meta.env.VITE_API_BASE_URL + '/forms' });
-  
+     const mdDown = useMediaQuery("down", "md");
 	//useEffect
 	React.useEffect(() => {
 		refetch();
 	},[]);
 
     // Column Definitions: Defines the columns to be displayed.
-    const [colDefs, setColDefs] = React.useState([
-		{ field: "name", cellRenderer : (params) => {
-			const rowData = params.data;
-			return 	<Box  sx={{ mt : 1 }}>
-				<Typography  sx={{ mb : -2, whiteSpace: "nowrap"}} fontSize={14}>{rowData.name}</Typography>
-			
-				<Typography color="text.secondary" sx={{ mb : 1}} variant="caption">
-					{rowData.alias}
-				</Typography>
-		</Box>
+    const [colDefs, setColDefs] = React.useState([]);
 
-		}},
-		{ field: "code"},
-        { field: "category"},
-        { field: "status", 	cellRenderer : (params) => {
 
-			const rowData = params.data;
-			const mapping = {
-				confirmed: {
-					label: "Confirmed",
-					icon: <CheckCircleIcon color="var(--mui-palette-success-main)" weight="fill" />,
-				},
-				closed: { label: "Closed", icon: <XCircleIcon color="var(--mui-palette-error-main)" weight="fill" /> },
-				pending: { label: "Pending", icon: <HourglassHighIcon color="var(--mui-palette-warning-main)" /> },
-				ongoing: { label: "Ongoing", icon: <HourglassHighIcon color="var(--mui-palette-success-main)" /> },
-			};
-			const { label, icon } = mapping[rowData.status] ?? { label: "Unknown", icon: null };
+	React.useEffect(() => {
 
-			return <Chip icon={icon} label={label} size="small" variant="outlined" />;
-		}},
+		setColDefs([
+			{ field: "name", minWidth: 300, cellRenderer : (params) => {
+				const rowData = params.data;
+				return 	<Box  sx={{ mt : 1 }}>
+						<Typography  sx={{ mb : -2, whiteSpace: "nowrap"}} fontSize={14}>{rowData.name}</Typography>
+						<Typography color="text.secondary" sx={{ mb : 1}} variant="caption">
+							{rowData.alias}
+						</Typography>
+					</Box>
+			}},
+			{ field: "code", hide : mdDown ? true : false},
+			{ field: "category", hide : mdDown ? true : false},
+			{ field: "status", hide : mdDown ? true : false, cellRenderer : (params) => {
 	
-		{ field: "action", cellRenderer : (params) => {
-			const rowData = params.data;
-			return <>
+				const rowData = params.data;
+				const mapping = {
+					confirmed: {
+						label: "Confirmed",
+						icon: <CheckCircleIcon color="var(--mui-palette-success-main)" weight="fill" />,
+					},
+					closed: { label: "Closed", icon: <XCircleIcon color="var(--mui-palette-error-main)" weight="fill" /> },
+					pending: { label: "Pending", icon: <HourglassHighIcon color="var(--mui-palette-warning-main)" /> },
+					ongoing: { label: "Ongoing", icon: <HourglassHighIcon color="var(--mui-palette-success-main)" /> },
+				};
+				const { label, icon } = mapping[rowData.status] ?? { label: "Unknown", icon: null };
+	
+				return <Chip icon={icon} label={label} size="small" variant="outlined" />;
+			}},
+		
+			{ field: "action", cellRenderer : (params) => {
+				const rowData = params.data;
+				return <>
+	
+					<Link 
+					sx={{ cursor : 'pointer', mr : 2 }}
+					onClick={() => {
+						navigate(paths.dashboard.forms.details(rowData.code))
+					}}>
+						View details
+					</Link>
+				</>
+			} }])
 
-				<Link 
-				sx={{ cursor : 'pointer', mr : 2 }}
-				onClick={() => {
-					navigate(paths.dashboard.forms.details(rowData.code))
-				}}>
-					View details
-				</Link>
-			</>
-		} }
-    ]);
+	},[forms,mdDown])
 
 	return <>
 	
