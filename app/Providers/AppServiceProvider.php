4ef00715d5b37040547ce45148e6667036cf9854
ZharifPuncak <zharif.rosli@puncakniaga.com.my>
2025-04-03 13:15:09 +0800

    Add email default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +22,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
+    {  
+
+        Schema::defaultStringLength(191);
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return env('APP_URL')."/e-forms/auth/update-password?token=$token&email={$notifiable->getEmailForPasswordReset()}";
         });
